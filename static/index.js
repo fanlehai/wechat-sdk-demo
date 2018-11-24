0c18f8fe05f1c1bcb7e571f0a20d63973768113b
fanlehai <fanlehai@163.com>
2018-11-24 11:29:34 +0800

    1. 修改链接

--- static/index.js
+++ static/index.js
@@ -63,26 +63,41 @@ wx.ready(function(){
    // 2. 分享接口
   // 2.1 监听“分享给朋友”，按钮点击、自定义分享内容及分享结果接口
   document.querySelector('#onMenuShareAppMessage').onclick = function () {
-    wx.onMenuShareAppMessage({
-      title: '互联网之子',
-      desc: '在长大的过程中，我才慢慢发现，我身边的所有事，别人跟我说的所有事，那些所谓本来如此，注定如此的事，它们其实没有非得如此，事情是可以改变的。更重要的是，有些事既然错了，那就该做出改变。',
-      link: 'http://movie.douban.com/subject/25785114/',
-      imgUrl: 'http://demo.open.weixin.qq.com/jssdk/images/p2166127561.jpg',
-      trigger: function (res) {
-        // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
-        alert('用户点击发送给朋友');
-      },
-      success: function (res) {
-        alert('已分享');
-      },
-      cancel: function (res) {
-        alert('已取消');
-      },
-      fail: function (res) {
-        alert(JSON.stringify(res));
-      }
-    });
-    alert('已注册获取“发送给朋友”状态事件');
+    // wx.onMenuShareAppMessage({
+    //   title: '互联网之子',
+    //   desc: '在长大的过程中，我才慢慢发现，我身边的所有事，别人跟我说的所有事，那些所谓本来如此，注定如此的事，它们其实没有非得如此，事情是可以改变的。更重要的是，有些事既然错了，那就该做出改变。',
+    //   link: 'http://movie.douban.com/subject/25785114/',
+    //   imgUrl: 'http://demo.open.weixin.qq.com/jssdk/images/p2166127561.jpg',
+    //   trigger: function (res) {
+    //     // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
+    //     alert('用户点击发送给朋友');
+    //   },
+    //   success: function (res) {
+    //     alert('已分享');
+    //   },
+    //   cancel: function (res) {
+    //     alert('已取消');
+    //   },
+    //   fail: function (res) {
+    //     alert(JSON.stringify(res));
+    //   }
+    // });
+    // alert('已注册获取“发送给朋友”状态事件');
+
+    // 自定义“分享给朋友”及“分享到QQ”按钮的分享内容（1.4.0）
+    wx.ready(function () {   //需在用户可能点击分享按钮前就先调用
+      wx.updateAppMessageShareData({ 
+          title: '互联网之子', // 分享标题
+          desc: '在长大的过程中，我才慢慢发现，我身边的所有事，别人跟我说的所有事', // 分享描述
+          link: 'http://movie.douban.com/subject/25785114/', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+          imgUrl: 'ttp://demo.open.weixin.qq.com/jssdk/images/p2166127561.jpg', // 分享图标
+          success: function () {
+            // 设置成功
+            alert('已分享');
+          }
+  });})
+
+
   };
 
     // 5 图片接口
